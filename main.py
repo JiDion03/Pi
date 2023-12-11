3f271eabf37f0a1f9049cd9b800a6af0920b657e
Horga Filip-Sebastian <132468926+JiDion03@users.noreply.github.com>
2023-12-11 19:47:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
 import hashlib
+import os
+import pygame.image
+
 
 from const import *
 
@@ -9,7 +12,7 @@ HEIGHT = 729
 
 BG_COLOR = (0, 255, 0)
 LINE_COLOR = (23, 145, 135)
-CROSS_COLOR = (66, 66, 66)
+CROSS_COLOR = (200, 66, 100)
 CIRCLE_COLOR = (239, 231, 200)
 FADE = (28, 170, 156)
 
@@ -71,11 +74,9 @@ class Board:
 
                 if isinstance(sqr, Board): sqr.render(surface)
         
-        # vertical lines
         pygame.draw.line(surface, LINE_COLOR, (self.dims.xcor + self.dims.sqsize, self.dims.ycor),                  (self.dims.xcor + self.dims.sqsize, self.dims.ycor + self.dims.size), self.linewidth)
         pygame.draw.line(surface, LINE_COLOR, (self.dims.xcor + self.dims.size - self.dims.sqsize, self.dims.ycor), (self.dims.xcor + self.dims.size - self.dims.sqsize, self.dims.ycor + self.dims.size), self.linewidth)
-        
-        # horizontal lines
+
         pygame.draw.line(surface, LINE_COLOR, (self.dims.xcor, self.dims.ycor + self.dims.sqsize),                  (self.dims.xcor + self.dims.size, self.dims.ycor + self.dims.sqsize), self.linewidth)
         pygame.draw.line(surface, LINE_COLOR, (self.dims.xcor, self.dims.ycor + self.dims.size - self.dims.sqsize), (self.dims.xcor + self.dims.size, self.dims.ycor + self.dims.size - self.dims.sqsize), self.linewidth)
 
@@ -89,8 +90,10 @@ class Board:
 
         sqr = self.squares[row][col]
 
+
         if not isinstance(sqr, Board):
             return sqr == 0 and self.active
+
 
         return sqr.valid_sqr(xclick, yclick)
 
@@ -104,6 +107,7 @@ class Board:
         sqr = self.squares[row][col]
 
         print('marking -> (', row, col, ')')
+
 
         if not isinstance(sqr, Board):
             self.squares[row][col] = player
@@ -120,9 +124,12 @@ class Board:
 
         sqr = self.squares[row][col]
 
+
         if not isinstance(sqr, Board):
 
+
             if sqr == 1:
+
                 ipos = (self.dims.xcor + (col * self.dims.sqsize) + self.offset, 
                         self.dims.ycor + (row * self.dims.sqsize) + self.offset)
                 fpos = (self.dims.xcor + self.dims.sqsize * (1 + col) - self.offset, 
@@ -135,6 +142,7 @@ class Board:
                         self.dims.ycor + (row * self.dims.sqsize) + self.offset)
                 pygame.draw.line(surface, CROSS_COLOR, ipos, fpos, self.linewidth)
             
+
             elif sqr == 2:
                 center = (self.dims.xcor + self.dims.sqsize * (0.5 + col),
                           self.dims.ycor + self.dims.sqsize * (0.5 + row))
@@ -143,9 +151,11 @@ class Board:
 
             return
 
+
         sqr.draw_fig(surface, xclick, yclick)
 
     def manage_win(self, surface, winner, onmain=False):
+
         transparent = pygame.Surface( (self.dims.size, self.dims.size) )
         transparent.set_alpha( ALPHA )
         transparent.fill( FADE )
@@ -154,25 +164,31 @@ class Board:
             surface.blit(transparent, (self.dims.xcor, self.dims.ycor))
         surface.blit(transparent, (self.dims.xcor, self.dims.ycor))
         
+
         if not onmain:
+
             if winner == 1:
+
                 ipos = (self.dims.xcor + self.offset, 
                         self.dims.ycor + self.offset)
                 fpos = (self.dims.xcor + self.dims.size - self.offset, 
                         self.dims.ycor + self.dims.size - self.offset)
                 pygame.draw.line(surface, CROSS_COLOR, ipos, fpos, self.linewidth + 7)
 
+
                 ipos = (self.dims.xcor + self.offset, 
                         self.dims.ycor + self.dims.size - self.offset)
                 fpos = (self.dims.xcor + self.dims.size - self.offset, 
                         self.dims.ycor + self.offset)
                 pygame.draw.line(surface, CROSS_COLOR, ipos, fpos, self.linewidth + 7)
+
 
             if winner == 2:
                 center = (self.dims.xcor + self.dims.size * 0.5,
                         self.dims.ycor + self.dims.size * 0.5)
 
                 pygame.draw.circle(surface, CIRCLE_COLOR, center, self.dims.size * 0.4, self.linewidth + 7)
+
 
         self.active = False
 
@@ -182,10 +198,12 @@ class Board:
 
         for row in range(DIM):
             for col in range(DIM):
-                  
+
+                   
                 sqr = self.squares[row][col]
 
                 if isinstance(sqr, Board) and sqr.active:
+
                     winner = sqr.check_draw_win(surface)
                     if winner: 
                         self.squares[row][col] = winner
@@ -195,7 +213,7 @@ class Board:
                 for c in range(DIM):
                     if self.squares[0][c] == self.squares[1][c] == self.squares[2][c] != 0:
                         color = CROSS_COLOR if self.squares[0][c] == 1 else CIRCLE_COLOR
-
+                        # draw win
                         ipos = (self.dims.xcor + self.dims.sqsize * (0.5 + c), 
                                 self.dims.ycor + self.offset)
                         fpos = (self.dims.xcor + self.dims.sqsize * (0.5 + c), 
@@ -229,10 +247,10 @@ class Board:
 
                     return self.squares[1][1]
 
-
+                # asc
                 if self.squares[2][0] == self.squares[1][1] == self.squares[0][2] != 0:
                     color = CROSS_COLOR if self.squares[1][1] == 1 else CIRCLE_COLOR
-
+                    # draw win
                     ipos = (self.dims.xcor + self.offset, 
                             self.dims.ycor + self.dims.size - self.offset)
                     fpos = (self.dims.xcor + self.dims.size - self.offset, 
@@ -294,18 +312,21 @@ class Game:
     def should_return_to_menu(self):
         return self.return_to_menu
 
+
+
 class Menu:
 
     def __init__(self):
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Menu')
-        pygame.font.init()  # Initialize the font module
+        pygame.font.init()  
         self.font = pygame.font.SysFont('monospace', 40)
         self.menu_options = ["Classic", "Ultimate", "Max"]
         self.selected_option = 0
 
     def draw_menu(self):
-        self.screen.fill(BG_COLOR)
+        self.screen.blit(background_image, (0, 0))
+        #self.screen.fill(BG_COLOR)
         for i, option in enumerate(self.menu_options):
             color = (255, 255, 255) if i == self.selected_option else (100, 100, 100)
             label = self.font.render(option, 1, color)
@@ -339,7 +360,8 @@ class StartMenu:
         self.selected_option = 0
 
     def draw_menu(self):
-        self.screen.fill(BG_COLOR)
+        #self.screen.fill(BG_COLOR)
+        self.screen.blit(background_image, (0, 0))
         for i, option in enumerate(self.menu_options):
             color = (255, 255, 255) if i == self.selected_option else (100, 100, 100)
             label = self.font.render(option, 1, color)
@@ -373,7 +395,8 @@ class ColorMenu:
         self.selected_color = 0
 
     def draw_menu(self):
-        self.screen.fill(BG_COLOR)
+        #self.screen.fill(BG_COLOR)
+        self.screen.blit(background_image, (0, 0))
         for i, color in enumerate(self.colors):
             text_color = (255, 255, 255) if i == self.selected_color else (100, 100, 100)
             label = self.font.render(color, 1, text_color)
@@ -405,11 +428,12 @@ class StartMenu:
         self.screen = screen
         pygame.font.init()
         self.font = pygame.font.SysFont('monospace', 40)
-        self.menu_options = ["Start Game", "Customize", "Quit"] 
+        self.menu_options = ["Start Game",  "Quit"]  
         self.selected_option = 0
 
     def draw_menu(self):
-        self.screen.fill(BG_COLOR)
+        #self.screen.fill(BG_COLOR)
+        self.screen.blit(background_image, (0, 0))
         for i, option in enumerate(self.menu_options):
             color = (255, 255, 255) if i == self.selected_option else (100, 100, 100)
             label = self.font.render(option, 1, color)
@@ -441,17 +465,48 @@ class LoginScreen:
         self.username = ""
         self.password = ""
         self.is_logging_in = True
-        self.username_selected = True 
+        self.username_selected = True  
         self.password_selected = False  
+        self.error_message = ""
+        self.error_font = pygame.font.SysFont('monospace', 30)
+
+        # List of user credentials (username and hashed password)
+        self.user_credentials = [
+            {"username": "user1", "password_hash": hashlib.sha256("password1".encode()).hexdigest()},
+            {"username": "user2", "password_hash": hashlib.sha256("password2".encode()).hexdigest()},
+            {"username": "filip", "password_hash": hashlib.sha256("parola".encode()).hexdigest()},
+            # Add more usernames and hashed passwords as needed
+        ]
+
+    def authenticate_user(self):
+        username = self.username.strip()
+        password = self.password.strip()
+
+        entered_password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+        for user in self.user_credentials:
+            if user["username"] == username and user["password_hash"] == entered_password_hash:
+                self.is_logging_in = False
+                return True
+
+        self.error_message = "Incorrect username or password"
+        self.username = ""
+        self.password = ""
+        return False
 
     def draw_login_screen(self):
-        self.screen.fill(BG_COLOR)
-
+        #self.screen.fill(BG_COLOR)
+        self.screen.blit(background_image, (0, 0))
 
         label_username = self.font.render(f"Username: {self.username}", 1, (255, 255, 255))
         label_password = self.font.render(f"Password: {len(self.password)*'*'}", 1, (255, 255, 255))
         self.screen.blit(label_username, (WIDTH // 2 - label_username.get_rect().width // 2, HEIGHT // 4))
         self.screen.blit(label_password, (WIDTH // 2 - label_password.get_rect().width // 2, HEIGHT // 2))
+
+    def draw_error_message(self):
+        if self.error_message:
+            error_label = self.error_font.render(self.error_message, 1, (255, 0, 0))
+            self.screen.blit(error_label, (WIDTH // 2 - error_label.get_rect().width // 2, HEIGHT // 2 + 50))
 
     def run_login_screen(self):
         while self.is_logging_in:
@@ -463,30 +518,29 @@ class LoginScreen:
                         elif len(self.password) > 0 and self.password_selected:
                             self.password = self.password[:-1]
                     elif event.key == pygame.K_RETURN:
-
-                        self.is_logging_in = False
+                        if self.authenticate_user():
+                            return not self.is_logging_in
                     elif event.key == pygame.K_ESCAPE:
                         self.is_logging_in = False
                     elif event.key == pygame.K_TAB:
-
                         self.username_selected = not self.username_selected
                         self.password_selected = not self.password_selected
                     elif event.unicode and (self.username_selected or self.password_selected):
-
                         if self.username_selected:
                             self.username += event.unicode
                         elif self.password_selected:
                             self.password += event.unicode
 
             self.draw_login_screen()
+            self.draw_error_message()
             pygame.display.update()
-
 
         return not self.is_logging_in
 
 
-
-
+background_image = pygame.image.load(r'C:\Users\filip\Downloads\geometric-Blue-Wallpaper-Free-Download.jpg')
+        # Resize the image to match the screen size
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
 class Main:
 
@@ -494,10 +548,14 @@ class Main:
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('ULTIMATE TIC TAC TOE')
+        
+        # Blit the image onto the screen
+        self.screen.blit(background_image, (0, 0))
         self.login_screen = LoginScreen(self.screen)
         self.start_menu = StartMenu(self.screen)
         self.game = None
-        self.logged_in = False 
+        self.logged_in = False  
+        
 
     def mainloop(self):
         while True:
@@ -515,13 +573,14 @@ class Main:
                     if mode is None:
                         continue
 
-                    if mode == 0:  # Classic
+                    if mode == 0:  
                         self.game = Game()
-                    elif mode == 1:  # Ultimate
+                    elif mode == 1:  
                         self.game = Game(ultimate=True, max=False)
-                    elif mode == 2:  # Max
+                    elif mode == 2:  
                         self.game = Game(ultimate=True,max=True)
-                    self.screen.fill(BG_COLOR)
+                    #self.screen.fill(BG_COLOR)
+                    self.screen.blit(background_image, (0, 0))
                     self.game.render_board(self.screen)
                     pygame.display.update()
 
@@ -562,14 +621,13 @@ class Main:
             pygame.display.update()
 
     def show_customize_menu(self):
-        global BG_COLOR  
+        #global BG_COLOR  
 
         customize_menu = Menu()
         customize_menu.menu_options = ["Select Background Color", "Go Back"]
         customize_menu.selected_option = 0
 
         selected_color = "Green"  
-    
 
         while True:
             for event in pygame.event.get():
@@ -581,7 +639,7 @@ class Main:
                     elif event.key == pygame.K_RETURN:
                         customize_option = customize_menu.selected_option
 
-                        if customize_option == 0:  # Select Background Color
+                        if customize_option == 0:  
                             color_menu = ColorMenu(self.screen)
                             selected_color = color_menu.run_menu()
 
@@ -593,7 +651,8 @@ class Main:
                                 BG_COLOR = (0, 0, 255)
 
                             
-                            self.screen.fill(BG_COLOR)
+                            #self.screen.fill(BG_COLOR)
+                            self.screen.blit(background_image, (0, 0))
                             self.game.render_board(self.screen)
                             pygame.display.update()
 
